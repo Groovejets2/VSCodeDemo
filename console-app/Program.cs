479bf7e0d5f40bb56d8f7ddf8fde130564e6e606
Jonathan Hughes <jonathan.hughes@skycity.co.nz>
2021-08-30 20:34:17 +1200

    Final code with debugger working in VS Code

---
+++ console-app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Library;
+
+namespace console_app
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"The answer is {new Thing().Get(19, 23)}");
+        Console.WriteLine("Press any key to continue . . . ");
+        Console.ReadKey(true);
+    }
+  }
+}
